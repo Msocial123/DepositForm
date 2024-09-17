e48bb75737d7f6d73725815b8efbacf894ed8ea5
Aryan Sahu <aryan.sahu@publicissapient.com>
2024-09-17 15:28:21 +0530

    final changes

--- server.js
+++ server.js
@@ -158,6 +158,6 @@ const server = http.createServer((req, res) => {
     }
 });
 
-server.listen(3000, () => {
-    console.log('Server running at http://localhost:3000');
+server.listen(3011, () => {
+    console.log('Server running at http://localhost:3011');
 });
